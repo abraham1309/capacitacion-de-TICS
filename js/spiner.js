124603446c1111227cab2d6312fe4450edf8f5f4
abraham1309 <superx1@live.com.mx>
2021-06-16 16:19:31 -0500

    Se subio la pagina completa

---
+++ js/spiner.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict"; 
+	
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+})(jQuery);
